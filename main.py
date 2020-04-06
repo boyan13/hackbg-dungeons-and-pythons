1f817ac6d359877ad9a9c7876c6fd807e37e1b85
SilviyaVasileva <60818785+SilviyaVasileva@users.noreply.github.com>
2020-04-06 16:57:33 +0300

    Bad interface

---
+++ main.py
@@ -0,0 +1,17 @@
+from Hero import Hero
+from Weapon import Weapon
+from Spell import Spell
+from Dungeon import Dungeon
+
+
+def main():
+    hero_name = input("Enter hero's name: ")
+    hero_title = input("Enter hero's title: ")
+    # dungeon = Dungeon()
+    """
+    move: up down left right (w s a d)
+    fight: spell weapon (b/f) -> if in range : m(spell)/ (weapon)
+    """
+
+if __name__ == '__main__':
+    main()
